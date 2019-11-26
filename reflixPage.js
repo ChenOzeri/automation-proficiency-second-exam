bd8dcbf7bb7be588c37bf57cbe32650fc81b1c49
Chen Ozeri <graceozeri@gmail.com>
2019-11-26 21:15:01 +0200

    solution for problomatic HTML

--- reflixPage.js
+++ reflixPage.js
@@ -9,14 +9,16 @@ class ReflixPage {
     async selectUser(userName) {
         try {
             let users = await selenium.findElementListBy("className", "user")
-            console.log (users)
+            // console.log (users)
             for (let name of users) {
                 let user = await selenium.getTextFromElement("className", "user", name)
-                console.log (user)
-                if (user== userName){
+                // console.log (user)
+                if (user == userName) {
                     await selenium.clickElement("className", "user")
-            }}
-            console.log(userName)
+                    return
+                }
+                console.log(user)
+            }
 
             // loop for findELEMENTS - > then loops until it finds the wanted string 
 
@@ -43,10 +45,11 @@ class ReflixPage {
                 console.log("wrong budget.")
             }
 
-            let plusButton = await selenium.findElementListBy("className", "fas fa-plus-circle")
-            for (plusButton = 0; plusButton < 2; plusButton++){
-                await selenium.clickElement("className", "fas fa-plus-circle")}
-            
+            let plusButton = await selenium.findElementListBy("className", "fa-plus-circle")
+            for (plusButton = 0; plusButton < 2; plusButton++) {
+                await selenium.clickElement("className", "fa-plus-circle")
+            }
+
 
             let decreasedBudget = await selenium.getTextFromElement("id", "budget")
             console.log("Movie budget is now " + decreasedBudget)
@@ -59,32 +62,55 @@ class ReflixPage {
 
             let minusButton = await selenium.findElementBy("className", "fas fa-minus-circle")
             for (minusButton = 0; minusButton < 2; minusButton++) {
-                await selenium.clickElement("className", "fas fa-minus-circle")}
-                let newMovieBudget = await selenium.getTextFromElement("id", "budget")
-                if (newMovieBudget.includes("10")) {
-                    console.log("Movie has been returned successfully")
-                }
-                else {
-                    console.log("Error: can't return the desired movies.")
-                }
-            
+                await selenium.clickElement("className", "fas fa-minus-circle")
+            }
+            let newMovieBudget = await selenium.getTextFromElement("id", "budget")
+            if (newMovieBudget.includes("10")) {
+                console.log("Movie has been returned successfully")
+            }
+            else {
+                console.log("Error: can't return the desired movies.")
+            }
+
 
         } catch (error) {
             console.error("Problem with orderAndReturnTwoMovies function " + error)
         }
     }
+
+    // async printMovieInfo(movieName) {
+    //     try {
+    //         let movies = await selenium.findElementListBy("className", "movie")
+    //         for (let m of movies) {
+    //             let movieText = await selenium.getTextFromElement("className", "movie", m)
+    //             console.log(movieText)
+    //             if (movieText.includes(movieName)){
+    //                 await selenium.clickElement("className", "movie")
+    //         }}
+
+    //         let tarzanText = await selenium.getTextFromElement("className", "movie-detail")
+    //         console.log(tarzanText)
+    //     }
+    //     catch (error) {
+    //         console.error("Problem with printMovieInfo function " + error)
+    //     }
+    //     await selenium.close()
+    // }
+    
     async printMovieInfo(movieName) {
         try {
-            let movies = await selenium.findElementListBy("className", "movie")
-            for (let m of movies) {
-                let movieText = await selenium.getTextFromElement("className", "movie", m)
-                console.log(movieText)
-                if (movieText.includes(movieName)){
-                    await selenium.clickElement("className", "movie")
-            }}
-
-            let tarzanText = await selenium.getTextFromElement("className", "movie-detail")
-            console.log(tarzanText)
+            await selenium.sleep(2000)
+            let movies = await selenium.findElementListBy("css", "div.movie > p")
+            let movies_click = await selenium.findElementListBy("css","div.movie > a")
+            for (let i = 0;i< movies.length;i++) {
+                if (await movies[i].getAttribute("innerText") === movieName) {
+                    await selenium.clickElement("", "", movies_click[i],null)
+                    await selenium.sleep(2000)
+                    let currMovie = await selenium.findElementBy("css","div.movie-detail > p")
+                    console.log(await currMovie.getText());
+                    break;
+                }
+            }
         }
         catch (error) {
             console.error("Problem with printMovieInfo function " + error)
@@ -92,8 +118,6 @@ class ReflixPage {
         await selenium.close()
 
     }
-
-
 
 }
 
